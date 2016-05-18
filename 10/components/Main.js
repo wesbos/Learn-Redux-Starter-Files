7a2c59bcab739de61f2cbc50e3f5b58653a96f8c
Didier Franc <mail@didierfranc.com>
2016-05-18 19:37:44 +0200

    Something cleaner

--- 10/components/Main.js
+++ 10/components/Main.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-const Main = React.createClass({
-  render() {
-    return (
-      <div>
-        <h1>
-          <Link to="/">Reduxstagram</Link>
-        </h1>
-        {React.cloneElement(this.props.children, this.props)}
-      </div>
-    )
-  }
-});
+const Main = (props) => (
+  <div>
+    <h1>
+      <Link to='/'>Reduxstagram</Link>
+    </h1>
+    {React.cloneElement(props.children, {...props})}
+  </div>
+);
 
 export default Main;
