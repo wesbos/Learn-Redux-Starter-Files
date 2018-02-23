f34bedb6931416ce29c2fe3a2620a2b9fc7bad23
DIVY <divyadeepsawhney@hotmail.com>
2018-02-23 19:48:23 +0530

    Add starter files for 12th video

---
+++ 12/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+import { routerReducer } from 'react-router-redux';
+
+import posts from './posts';
+import comments from './comments';
+
+const rootReducer = combineReducers({posts, comments, routing: routerReducer });
+
+export default rootReducer;
