60d3a84e03b851476c938cd43e4d514823a36be0
DIVY <divyadeepsawhney@hotmail.com>
2018-02-24 14:49:03 +0530

    Add final files

---
+++ final/reducers/comments.js
@@ -0,0 +1,35 @@
+function postComments(state = [], action) {
+  switch(action.type){
+    case 'ADD_COMMENT':
+      // return the new state with the new comment
+      return [...state,{
+        user: action.author,
+        text: action.comment
+      }];
+    case 'REMOVE_COMMENT':
+      // we need to return the new state without the deleted comment
+      return [
+        // from the start to the one we want to delete
+        ...state.slice(0,action.i),
+        // after the deleted one, to the end
+        ...state.slice(action.i + 1)
+      ]
+    default:
+      return state;
+  }
+  return state;
+}
+
+function comments(state = [], action) {
+  if(typeof action.postId !== 'undefined') {
+    return {
+      // take the current state
+      ...state,
+      // overwrite this post with a new one
+      [action.postId]: postComments(state[action.postId], action)
+    }
+  }
+  return state;
+}
+
+export default comments;
---
+++ final/store.js
@@ -0,0 +1,32 @@
+import { createStore, compose } from 'redux';
+import { syncHistoryWithStore} from 'react-router-redux';
+import { browserHistory } from 'react-router';
+
+// import the root reducer
+import rootReducer from './reducers/index';
+
+import comments from './data/comments';
+import posts from './data/posts';
+
+// create an object for the default data
+const defaultState = {
+  posts,
+  comments
+};
+
+const enhancers = compose(
+  window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
+);
+
+const store = createStore(rootReducer, defaultState, enhancers);
+
+export const history = syncHistoryWithStore(browserHistory, store);
+
+if(module.hot) {
+  module.hot.accept('./reducers/',() => {
+    const nextRootReducer = require('./reducers/index').default;
+    store.replaceReducer(nextRootReducer);
+  });
+}
+
+export default store;
