71975c7931dfb2ff5f873a1decf982b8d630750d
Florestan Lefevre <florestanlefevre@gmail.com>
2018-04-23 14:24:53 -0700

    Create Redux actions

---
+++ learn-redux/client/actions/actionCreators.js
@@ -0,0 +1,26 @@
+// increment likes
+export function increment(index) {
+  return {
+    type: 'INCREMENT_LIKES',
+    index
+  }
+}
+
+// add comment
+export function addComment(postId, author, comment) {
+  return {
+    type: 'ADD_COMMENT',
+    postId,
+    author,
+    comment
+  }
+}
+
+// remove comment
+export function removeComment(postId, i) {
+  return {
+    type: 'REMOVE_COMMENT',
+    i,
+    postId
+  }
+}
