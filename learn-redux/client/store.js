44e80b0fdc799a3a81d2ca86ddb6a814207acd76
Gaorieh Isaac <gaorieh@hotmail.com>
2018-09-07 17:52:59 +0200

    video 18

--- learn-redux/client/store.js
+++ learn-redux/client/store.js
@@ -14,7 +14,15 @@ const defaultState = {
 };
 
 const store = createStore(rootReducer, defaultState);
+
 export const history = syncHistoryWithStore(browserHistory, store);
+
+if (module.hot) {
+    module.hot.accept('./reducers', () => {
+        const nextRootReducer = require('./reducers/index').default;
+        store.replaceReducer(nextRootReducer);
+    })
+}
 
 export default store;
 
