9fb468f83de28e7e723ebcdefe933eb0477414d8
Gaorieh Isaac <gaorieh@hotmail.com>
2018-09-07 11:32:41 +0200

    video 10

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -4,7 +4,7 @@ import { render } from 'react-dom';
 // import CSS
 import css from './styles/style.styl';
 //import Main component
-import Main from './components/Main';
+import App from './components/App';
 import Single from './components/Single';
 import PhotoGrid from './components/PhotoGrid';
 
@@ -16,7 +16,7 @@ import store, { history } from './store';
 const router = (
     <Provider store={store}>
         <Router history={history}>
-            <Route path="/" component={Main}>
+            <Route path="/" component={App}>
                 <IndexRoute component={PhotoGrid}></IndexRoute>
                 <Route path="/view/:postId" component={Single}></Route>
             </Route>
