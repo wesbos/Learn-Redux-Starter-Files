b7710b0e1c569d4f36a4c030c445535af6d56bf0
Gaorieh Isaac <gaorieh@hotmail.com>
2018-09-07 17:37:18 +0200

    video 16

--- learn-redux/client/components/Comments.js
+++ learn-redux/client/components/Comments.js
@@ -8,7 +8,7 @@ const Comments = React.createClass({
                 <p>
                     <strong>{comment.user}</strong>
                     {comment.text}
-                    <button className="remove-comment">&times;</button>
+                    <button className="remove-comment" onClick={this.props.removeComment.bind(null, this.props.params.postId, i)}>&times;</button>
                 </p>
 
             </div>
@@ -21,6 +21,7 @@ const Comments = React.createClass({
         const comment = this.refs.comment.value;
         console.log(postId + ' : ' + author + '  :  ' + comment);
         this.props.addComment(postId, author, comment);
+        this.refs.commentForm.reset();
     },
 
     render() {
--- learn-redux/client/reducers/comments.js
+++ learn-redux/client/reducers/comments.js
@@ -1,9 +1,31 @@
-// a reducer takes in two things:
-//1. the actions (info about what happened)
-//2. a copy of current state
+function postComments(state = [], action) {
+    switch (action.type) {
+        case 'ADD_COMMENT':
+            return [...state, {
+                user: action.author,
+                text: action.comment
+            }]
+        case 'REMOVE_COMMENT':
+            const i = action.i;
+            console.log('remove comment: ' + [i]);
+            return [...state.slice(0, i),
+            ...state.slice(i + 1)
+            ];
+        default:
+            return state;
+    }
+
+    return state;
+
+}
 
 function comments(state = [], action) {
-
+    if (typeof action.postId !== 'undefined') {
+        return {
+            ...state,
+            [action.postId]: postComments(state[action.postId], action)
+        }
+    }
     return state;
 
 }
