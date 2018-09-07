ef6f82582cbcc254e00728ff536d59e841a1b0b9
Gaorieh Isaac <gaorieh@hotmail.com>
2018-09-07 13:00:47 +0200

    video 12

--- learn-redux/client/components/Photo.js
+++ learn-redux/client/components/Photo.js
@@ -23,7 +23,7 @@ const Photo = React.createClass({
                     <p>
                         {post.caption}
                         <div className="control-buttons">
-                            <button className="likes">&hearts;{post.likes}</button>
+                            <button onClick={this.props.increment.bind(null, i)} className="likes">&hearts;{post.likes}</button>
                             <Link className="button" to={`/view/${post.code}`}>
                                 <span className="comment-count"><span className="speech-bubble"></span>
                                     {comments[post.code] ? comments[post.code].length : 0}
--- learn-redux/client/reducers/posts.js
+++ learn-redux/client/reducers/posts.js
@@ -3,9 +3,19 @@
 //2. a copy of current state
 
 function posts(state = [], action) {
-    console.log('The Post will change');
-    console.log(state, action);
-    return state;
+    switch (action.type) {
+        case 'INCREMENT_LIKES':
+            console.log('increment Likes!');
+            const i = action.index;
+            return [
+                ...state.slice(0, i),
+                { ...state[i], likes: state[i].likes + 1 },
+                ...state.slice(i + 1)
+            ]
+        default:
+            return state;
+    }
+
 }
 
 export default posts;
