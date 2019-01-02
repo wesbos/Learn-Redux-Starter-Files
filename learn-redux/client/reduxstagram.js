54f9b1d92606a16a5241bc650ebbd951186ea76b
Aditya Shah <aditya.shah7023@gmail.com>
2019-01-02 00:30:11 -0500

    Added component structure

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,1 +1,7 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+import Main from './components/Main';
+
+import css from './styles/style.styl';
+
+render(<Main/>, document.getElementById('root'));
