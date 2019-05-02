b01e08e61bd69e6ecbf172c7bb03e0ed60e6479e
Tommi Rautava <tommi.rautava@ge.com>
2019-05-02 15:00:12 +0300

    13

---
+++ learn-redux/client/components/Comments.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Comments = class extends React.Component {
+  render() {
+    return <div className="comment">Comments</div>;
+  }
+};
+
+export default Comments;
--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import Photo from './Photo';
+import Comments from './Comments';
 
 const Single = class extends React.Component {
   render() {
-    return <div className="single-photo">Single</div>;
+    const i = this.props.posts.findIndex(
+      (post) => post.code === this.props.params.postId
+    );
+
+    const post = this.props.posts[i];
+
+    return (
+      <div className="single-photo">
+        <Photo i={i} post={post} {...this.props} />
+        <Comments />
+      </div>
+    );
   }
 };
 
