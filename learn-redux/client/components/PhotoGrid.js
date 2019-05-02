066b0078d1c01bcbcc50f9c49a5905543dfce060
Tommi Rautava <tommi.rautava@ge.com>
2019-05-02 14:02:03 +0300

    11

--- learn-redux/client/components/PhotoGrid.js
+++ learn-redux/client/components/PhotoGrid.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import Photo from './Photo';
 
 const PhotoGrid = class extends React.Component {
   render() {
-    return <div className="photo-grid">Grid</div>;
+    return (
+      <div className="photo-grid">
+        {this.props.posts.map((post, i) => (
+          <Photo key={i} {...this.props} i={i} post={post} />
+        ))}
+      </div>
+    );
   }
 };
 
