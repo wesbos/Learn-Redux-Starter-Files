70bc9026cade917f9ba1aeeb562ec415f35d332a
Tommi Rautava <tommi.rautava@ge.com>
2019-05-02 13:36:29 +0300

    10

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -6,7 +6,7 @@ import { render } from 'react-dom';
 import css from './styles/style.styl';
 
 // Import Components
-import Main from './components/Main';
+import App from './components/App';
 import Single from './components/Single';
 import PhotoGrid from './components/PhotoGrid';
 
@@ -18,7 +18,7 @@ import store, { history } from './store';
 const router = (
   <Provider store={store}>
     <Router history={history}>
-      <Route path="/" component={Main}>
+      <Route path="/" component={App}>
         <IndexRoute component={PhotoGrid} />
         <Route path="/view/:postId" component={Single} />
       </Route>
