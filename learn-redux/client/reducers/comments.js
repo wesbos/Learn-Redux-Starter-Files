75b08bf9d460d452bef50ec269ea5b225960d7ab
Kris Ringler <kmringler@gmail.com>
2020-04-28 18:07:57 -0700

    action reducers added

---
+++ learn-redux/client/reducers/comments.js
@@ -0,0 +1,7 @@
+function comments(state = [], action) {
+  console.log(state, action)
+  return state;
+}
+
+
+export default comments;
--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -13,16 +13,19 @@ import PhotoGrid from './components/PhotoGrid';
 //import react router deps
 
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
-
+import { Provider } from 'react-redux';
+import store, { history } from './store';
 const router = (
-  <Router history={browserHistory}>
-    <Route path="/" component={Main}>
-      <IndexRoute component={PhotoGrid}></IndexRoute>
-      <Route path="/view/:postId" component={Single}>
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={Main}>
+        <IndexRoute component={PhotoGrid}></IndexRoute>
+        <Route path="/view/:postId" component={Single}>
 
+        </Route>
       </Route>
-    </Route>
-  </Router>
+    </Router>
+  </Provider>
 
 )
 
