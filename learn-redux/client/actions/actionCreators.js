eefff07589f3031ac18eb49d4c092ad6d1848da0
Kris Ringler <kmringler@gmail.com>
2020-04-28 17:49:32 -0700

    action functions added - add comment, remove comment, increment likes

---
+++ learn-redux/client/actions/actionCreators.js
@@ -0,0 +1,28 @@
+//increment a like
+export function increment(index) {
+  return {
+    type: 'INCREMENT_LIKES',
+    index
+
+  }
+}
+
+//add comment
+export function addComment(postId, author, comment) {
+  return {
+    type: 'ADD_COMMENT',
+    postId,
+    author,
+    comment
+  }
+}
+
+//remove comment
+
+export function removeComment(postId, i) {
+  return {
+    type: 'REMOVE_COMMENT',
+    i,
+    postId
+  }
+}
