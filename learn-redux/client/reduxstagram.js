be346452896bfdb1c7cc19b383ed20bfb1309787
Tim DeCillis <timdecillis@gmail.com>
2023-09-16 19:19:04 -0700

    Added react router

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -4,5 +4,21 @@ import {render} from 'react-dom';
 import css from './styles/style.styl';
 
 import Main from './components/Main';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
 
-render(<Main/>, document.getElementById('root'));
+import { Router, Route, IndexRoute, browserHistory} from 'react-router'
+
+const router = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+      <IndexRoute component={PhotoGrid}>
+        <Route path='/view/:postId'>
+
+        </Route>
+      </IndexRoute>
+    </Route>
+  </Router>
+)
+
+render(router, document.getElementById('root'));
